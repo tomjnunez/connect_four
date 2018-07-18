406756f5aa4ac111433af5ec9c1192adffcfe666
Tom Nunez <tomjnunez@gmail.com>
2018-07-18 17:42:42 -0600

    finally prints board

---
+++ test/board_test.rb
@@ -0,0 +1,16 @@
+
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_prints_board
+    board = Board.new
+
+    assert_equal 
+  end
+end
